6df9caf8e9802b82ef32b2060e45bee04b03929e
Mohamed Shawkat <mohamedshawkat95@gmail.com>
2019-03-24 14:01:27 +0200

    Add Order Front End

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
  end
   get 'welcome/showFriends'
   # root 'welcome#showFriends'
+  resources :orders
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
