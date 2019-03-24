ad3fa25b1a73c91fcb47c7ec8db704d5810f4dd1
alaatarek95 <alaatarek95@gmail.com>
2019-03-24 11:29:12 +0200

    groups users relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ belongs_to :friend
 has_many :inverse_friends, :class_name => "Friend", :foreign_key => "friend_id"
 has_many :notifications
 has_and_belongs_to_many :orders
-has_and_belongs_to_many :groups
+has_many :members
 
 
 end
