3e4cfce7a3612348e70bbb8b8d19f7e368d39f36
“Esraa <esraadarweesh1995@gmail.com>
2019-03-24 15:18:41 +0200

    login with facebook

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'devise'
 gem 'omniauth-facebook'
+# gem 'google_sign_in'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook]
+         :omniauthable, :omniauth_providers => [:facebook ]
 
 
 has_many :friends
