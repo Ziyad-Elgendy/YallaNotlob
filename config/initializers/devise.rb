57747531dece347cd83c6b2e8a11d01165e37f67
“Esraa <esraadarweesh1995@gmail.com>
2019-03-24 12:10:44 +0200

    login with facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,5 +297,5 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-  config.omniauth :facebook, '256988478584927', '9553081b264710004c4103ba68bed77f', callback_url: "http://localhost:3000/users/auth/facebook/callback"
+  config.omniauth :facebook, '256988478584927', '9553081b264710004c4103ba68bed77f', callback_url: "https://yallanotlob39.herokuapp.com/users/auth/facebook/callback"
 end
