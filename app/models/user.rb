44efe32acfb6a57b375a33e2d1063c0c24092f0f
alaatarek95 <alaatarek95@gmail.com>
2019-03-25 06:59:36 +0200

    friends error solved

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
          :omniauthable, :omniauth_providers => [:facebook , :google_oauth2]
 
 
-# has_many :friends
-# belongs_to :friend
+has_many :friends
+belongs_to :friend
 has_many :inverse_friends, :class_name => "Friend", :foreign_key => "friend_id"
 has_many :notifications
 has_many :members
