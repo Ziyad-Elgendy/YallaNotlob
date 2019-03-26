ed8b535858abb1ee73be95b5345c6ff65aeb59e3
Mohamed Shawkat <mohamedshawkat95@gmail.com>
2019-03-26 17:29:48 +0200

    fixing conflict

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,18 +3,17 @@ class FriendsController < ApplicationController
     layout 'application'
 
     def create
-        user = User.find_by email: params[:email]
-        if (Friend.where(:friend_id => user.id, :user_id => current_user.id).count > 0) 
-          flash[:error] = "Unable to add friend."
-            redirect_to friends_path
+      user = User.find_by email: params[:email]
+      if (Friend.where(:friend_id => user.id, :user_id => current_user.id).count > 0) 
+        flash[:error] = "Unable to add friend."
+          redirect_to friends_path
+      else
+        if Friend.create(friend_id: user.id, user_id: current_user.id)
+          flash[:notice] = "Added friend."
+          redirect_to friends_path
         else
-          if Friend.create(friend_id: user.id, user_id: current_user.id)
-            flash[:notice] = "Added friend."
-            redirect_to friends_path
-          else
-            flash[:error] = "Unable to add friend."
-            redirect_to friends_path
-          end
+          flash[:error] = "Unable to add friend."
+          redirect_to friends_path
         end
       end
     end
