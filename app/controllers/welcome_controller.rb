23275645ee3f067fa72c2d92c4cbb730af932728
alaatarek95 <alaatarek95@gmail.com>
2019-03-28 16:44:22 +0200

    limit friends activities

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @activities = PublicActivity::Activity.where(:owner_id => [current_user.friends.ids])
+    @activities = PublicActivity::Activity.where(:owner_id => [current_user.friends.ids]).last(4)
     @latesOrders = Order.where(:user_id => current_user.id).last(4)
     return @activities, @latesOrders
   end
