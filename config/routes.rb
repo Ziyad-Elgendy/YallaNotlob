b5aefe059b5f58f5bf4f085b4b975328d2e74bf3
ziyadelgendy <ziyad.a.elgendy@gmail.com>
2019-03-28 11:12:18 +0200

    add friend and delete friend in group

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,11 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  resources :groups
+  resources :groups do
+    post 'getName'
+    post 'addFriend'
+    delete 'deletefriend'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
