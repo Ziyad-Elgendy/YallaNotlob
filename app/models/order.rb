5e14deb0bfe21c175182090b9463342d586608b2
nouran <nouranougazy@gmail.com>
2019-03-28 10:35:45 +0200

    Delete items

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
-  has_many :user_orders
-  has_many :order_items
+  has_many :user_orders 
+  has_many :order_items 
   has_one_attached :img
   belongs_to :user
 end
