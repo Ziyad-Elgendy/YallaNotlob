b8ce2904bc73c6547e2ec4396091102d99d4a35d
ziyadelgendy <ziyad.a.elgendy@gmail.com>
2019-03-29 07:04:47 +0200

    fix error

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,9 +3,6 @@ class GroupsController < ApplicationController
   def index
       @groups = Group.where(user_id: current_user.id)
       @group = Group.new
-      # @group = Group.destroy
-      # @group_s = Group.find(params[:id])
-      # @friends = @group_s.users
   end
       
   def listG
@@ -21,32 +18,28 @@ class GroupsController < ApplicationController
 
   def show
     @groups = Group.where(user_id: current_user.id)
-    @group = Group.new
-    
+    @group = Group.new   
     @group = Group.find(params[:id])
     $search = @group.id
     @group_s = Group.find(params[:id])
     @members = Member.where(group_id: params[:id])
-
     @friends = @group_s.users
     render :index
-
   end 
 
   def new
     @group = Group.new
   end
+
   def create
     if group_params[:name] == ""
       respond_to do |format|
-        format.html { redirect_to groups_path, notice: 'Group was Empty Cant created.' }
+        format.html { redirect_to groups_path, notice: 'Insert the name of the group.' }
       end
-
     else
       if current_user.groups.exists? name: group_params[:name]
-				#this group is already exist
         respond_to do |format|
-          format.html { redirect_to groups_path, notice: 'Group was Found .' }
+          format.html { redirect_to groups_path, notice: 'Group already exist .' }
         end
       else
         @group = Group.new(group_params.merge(user_id: current_user))
@@ -85,7 +78,7 @@ class GroupsController < ApplicationController
         goto_groups_page
       else
         respond_to do |format|
-          format.html { redirect_to groups_path, notice: 'You cannot delete the group ' }
+          format.html { redirect_to groups_path, notice: 'You cannot delete this group ' }
           format.json { head :no_content }
         end
       end
@@ -99,39 +92,29 @@ class GroupsController < ApplicationController
     end
   end      
 
-
   def addFriend
-    #Now i have this friend data from the data base and i already have the groupId
-    #get the friend name
 		@friendName = params[:name]	
-    #check if it is null
     if @friendName.empty?
       respond_to do |format|
-        format.html { redirect_back fallback_location: root_path, notice: 'Please insert some data !' }
+        format.html { redirect_back fallback_location: root_path, notice: 'Please insert the name of your friend !' }
         format.json { head :no_content }
       end
     else
-      #check if it is wrong type
-      if User.exists? name: @friendName
-        #it is really a user
-        #check if he is tring to add himself
+      if User.exists? name: @friendNames
         if @friendName == current_user.name	
           respond_to do |format|
-            format.html { redirect_back fallback_location: root_path, notice: 'Cant insert your self!' }
+            format.html { redirect_back fallback_location: root_path, notice: 'You cannot add yourself !' }
             format.json { head :no_content }
           end
         else	
-        #check if the user exist in the group itself before
           @fid = User.find_by(name: params[:name])
           @current_group = Group.find(params[:groupId])
           if @current_group.members.exists? user: @fid.id  	  
-            puts "why you are tring to add the same friend"
             respond_to do |format|
-              format.html { redirect_back fallback_location: root_path, notice: 'This Friend is already assigned to this group !!' }
+              format.html { redirect_back fallback_location: root_path, notice: 'This Friend is already member of this group !' }
               format.json { head :no_content }
             end
           else
-            #check if this user in friendship list or not
             @fid = User.find_by(name: params[:name])
             @user = current_user
             if @user.friends.exists? friend: @fid.id
@@ -139,19 +122,18 @@ class GroupsController < ApplicationController
             @gid = params[:groupId]
             @group = @fid.members.create(group_id: @gid)
                 respond_to do |format|
-                  format.html { redirect_back fallback_location: root_path, notice: 'This Friend is Add !!' }
+                  format.html { redirect_back fallback_location: root_path, notice: 'Friend is add successfully !' }
                   format.json { head :no_content }
                 end
             else
               respond_to do |format|
-                format.html { redirect_back fallback_location: root_path, notice: 'This user is not currently in your friend list! !!' }
+                format.html { redirect_back fallback_location: root_path, notice: 'This user is not one of your friends !' }
                 format.json { head :no_content }
               end               
             end
           end					    
         end	
       else
-      #it is a robot
         respond_to do |format|
           format.html { redirect_back fallback_location: root_path, notice: 'This user is not exist !' }
           format.json { head :no_content }
@@ -160,15 +142,11 @@ class GroupsController < ApplicationController
     end		
   end
 
-
   def deletefriend
     @group_member = Member.find_by(group_id: params[:group_id], user_id: params[:user_id])
-    name = User.find(params[:user_id]).name
-    goup = Group.find(params[:group_id]).name
-
     if @group_member.destroy
       respond_to do |format|
-        format.html { redirect_back fallback_location: root_path, notice: "Freind #{name} in #{goup} Group was successfully destroyed." }
+        format.html { redirect_back fallback_location: root_path, notice: "Freind was successfully deleted." }
         format.json { head :no_content }
       end
     end
