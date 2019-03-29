905accd065dc61beb0aa5ce5ba1c706003e672b8
Mohamed Shawkat <mohamedshawkat95@gmail.com>
2019-03-29 18:26:36 +0200

    fix web socket heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,8 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
+  config.action_cable.url = 'wss://yallanotlob39.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://yallanotlob39.herokuapp.com', /http:\/\/yallanotlob39.*/ ]
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
